e31cff09c868cb28b69b4a6da063f29ec0cdde48
Deng ZheWen <1920902706@qq.com>
2025-02-14 14:55:50 +0800

    First commit lab01

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -2,6 +2,22 @@
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* your code here */
+    node *hare;
+    node *tortoise;
+    hare = head;
+    tortoise = head;
+    if (head == NULL) {
+	    return 0;
+    }
+    if(head->next == NULL){
+	    return 0;
+    }
+    while (hare->next != NULL && hare->next->next != NULL){
+	   hare = hare->next->next;
+           tortoise = tortoise->next; 
+           if (hare == tortoise) {
+	        return 1;
+           }
+    }
     return 0;
 }
